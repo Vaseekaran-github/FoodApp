ddb8a01724f62a339684f660c01af7933d33354e
Vaseekaran <70377283+Vaseekaran-github@users.noreply.github.com>
2024-01-18 22:00:36 +0530

    third commit

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 # Create your views here.
 
 
 def register(Request):
 
     if Request.method == "POST":
-        form = UserCreationForm(Request.POST)
+        form = RegisterForm(Request.POST)
         if form.is_valid():
+            form.save()
             username=form.cleaned_data.get('username')
             messages.success(Request,f'Welcome {username},your account is successfully created')
-            return redirect('index')
+            return redirect('login')
     else:
-        form=UserCreationForm()
+        form=RegisterForm()
     return render (Request,'users/register.html',{'form':form})
     
 
